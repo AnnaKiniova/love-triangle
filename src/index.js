ffe903524b5fb84dc8122b0fe183523dff892df3
Anna Kiniova <anna.kiniova@gmail.com>
2019-02-23 23:55:51 +0300

    solution task 2

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let population=preferences.length;
+  let count=0;
+  for(let i=0; i<population; i++) {
+    let firstLove=preferences[i];
+    let secondLove=preferences[firstLove-1];
+    let thirdLove=preferences[secondLove-1];
+    if ((thirdLove==i+1) && (thirdLove!=secondLove) &&(secondLove!=firstLove)) {
+      count++;
+    };
+  }
+  return count/3;
 };
